47426bd0a9b23a43aed5c9dc1d44ebcf80ba26a4
rohan <135570967+Imrohan007@users.noreply.github.com>
2025-09-30 16:02:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "ROHAN BHOJ",
+  title: "FRONTEND",
+  email: "rohanbhoj123456gmail.com",
+  gitHub: "",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
